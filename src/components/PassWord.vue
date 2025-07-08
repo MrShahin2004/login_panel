c8d97429cef91cfc4092952ed692487c295b4dde
Mr Shahin <hashmyshahin@gmail.com>
2025-07-08 20:58:14 +0330

    Added the HTML.

--- src/components/PassWord.vue
+++ src/components/PassWord.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div class="password-input" id="password-input">
+    <input dir="rtl" id="pass-input" type="password">
+    <div class="label-section" id="label-section">
+      <label for="text-input">گذر واژه</label>
+    </div>
+  </div>
 </template>
 
 <script></script>
