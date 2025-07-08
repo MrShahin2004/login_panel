27d7a443ea407b8edbdcaecdb36559466f829a84
Mr Shahin <hashmyshahin@gmail.com>
2025-07-08 20:58:21 +0330

    Added the styling.

--- src/components/PassWord.vue
+++ src/components/PassWord.vue
@@ -9,4 +9,37 @@
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+.password-input {
+  background-color: #f3f4f6;
+  padding: 0.5rem;
+  height: fit-content;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  width: 90%;
+}
+
+.label-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+label {
+  text-align: center;
+  font-size: 16px;
+}
+
+input {
+  font-family: "B Nazanin", sans-serif;
+  background-color: #dbeafe;
+  border: 0;
+  border-radius: 6px;
+  padding: 0.5rem;
+  font-size: 16px;
+}
+
+input:focus {
+  outline: none;
+}
+</style>
