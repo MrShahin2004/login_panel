ddb0a8507c274763defe11a85cf6a8f415bf41ac
Mr Shahin <hashmyshahin@gmail.com>
2025-07-08 20:52:13 +0330

    Added the styling.

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -1,7 +1,47 @@
 <template>
-  <div></div>
+  <div class="username-input" id="username-input">
+    <input dir="rtl" id="text-input" type="text">
+    <div class="label-section" id="label-section">
+      <label for="text-input">نام کاربری</label>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+</script>
 
-<style scoped></style>
+<style scoped>
+.username-input {
+  background-color: #f3f4f6;
+  padding: 0.5rem;
+  height: fit-content;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  width: 90%;
+}
+
+.label-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+label {
+  text-align: center;
+  font-size: 16px;
+}
+
+input {
+  font-family: "B Nazanin", sans-serif;
+  background-color: #dbeafe;
+  border: 0;
+  border-radius: 6px;
+  padding: 0.5rem;
+  font-size: 16px;
+}
+
+input:focus {
+  outline: none;
+}
+</style>
+
