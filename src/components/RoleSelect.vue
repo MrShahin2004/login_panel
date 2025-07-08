ef998df5388a7651bff1411dfe9c804e6e4de91a
Mr Shahin <hashmyshahin@gmail.com>
2025-07-08 13:45:45 +0330

    Added a select box with 3 options.

--- src/components/RoleSelect.vue
+++ src/components/RoleSelect.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="body-inputs" title="body-inputs">
+    <select id="select-input">
+      <option value="Choose">انتخاب کنید</option>
+      <option value="Choose">کاربر ثبات</option>
+      <option value="Choose">کاربر اداره کل</option>
+    </select>
+  </div>
 </template>
 
 <script></script>
