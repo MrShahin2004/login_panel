33d9621fc11871fde72312735e4d31f0ceb57383
Mr Shahin <hashmyshahin@gmail.com>
2025-07-08 13:29:23 +0330

    Deleted the test components.

--- src/components/FirstComp.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>FirstComp</h1>
-  </div>
-</template>
-
-<script></script>
-
-<style></style>
--- src/components/SecondComp.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>SecondComp</h1>
-  </div>
-</template>
-
-<script></script>
-
-<style></style>
