02fbcc77134fddbdbeaf9a937a366f13bc85927a
Mr Shahin <hashmyshahin@gmail.com>
2025-07-08 14:35:00 +0330

    Added the styles of the elements.

--- src/components/RoleSelect.vue
+++ src/components/RoleSelect.vue
@@ -11,6 +11,48 @@
   </div>
 </template>
 
-<script></script>
+<script>
+</script>
 
-<style></style>
+<style scoped>
+.select-box {
+  background-color: #f3f4f6;
+  padding: 0.5rem;
+  height: fit-content;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  width: 90%;
+  border-top-right-radius: 6px;
+  border-top-left-radius: 6px;
+}
+
+.label-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+label {
+  text-align: center;
+  font-size: 16px;
+}
+
+select {
+  font-family: "B Nazanin", sans-serif;
+  background-color: #dbeafe;
+  border: 0;
+  border-radius: 6px;
+  padding: 0.5rem;
+  font-size: 16px;
+}
+
+select:focus {
+  outline: none;
+}
+
+option {
+  background-color: white;
+  border: 0;
+  border-radius: 4px;
+}
+</style>
