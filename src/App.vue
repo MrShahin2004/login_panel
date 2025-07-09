05be9a76be07b9456c630136a89a3cd6d530884c
Mr Shahin <hashmyshahin@gmail.com>
2025-07-09 21:09:32 +0330

    Added the Tailwind classes.

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@
                 flex justify-center items-center relative">
       <div class="w-[500px] h-[500px] bg-[var(--body-bg)]
                   flex flex-col justify-between items-center rounded-[10px]">
-        <p class="body-title" dir="rtl">ورود به سامانه کشوری برون سپاری</p>
+        <p class="body-title text-[15px] bg-[var(--title-bg)]
+                  w-[90%] box-border h-fit rounded-[6px]" dir="rtl">ورود به سامانه کشوری برون سپاری</p>
         <div class="body-inputs">
           <RoleSelect/>
           <UserName/>
