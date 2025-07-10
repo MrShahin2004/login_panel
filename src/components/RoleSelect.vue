4fd544a26af49ed64fcb98669848d4d71686b794
Mr Shahin <hashmyshahin@gmail.com>
2025-07-10 08:34:30 +0330

    "rounded" was unused here.

--- src/components/RoleSelect.vue
+++ src/components/RoleSelect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="select-box bg-[#f3f4f6] h-fit grid grid-cols-[3fr_1fr] w-[90%] rounded-t-[6px_6px]">
     <select class="select-input bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0" dir="rtl">
-      <option class="bg-[white] border-0 rounded-[4px]" value="Choose">انتخاب کنید</option>
-      <option class="bg-[white] border-0 rounded-[4px]" value="Choose">کاربر ثبات</option>
-      <option class="bg-[white] border-0 rounded-[4px]" value="Choose">کاربر اداره کل</option>
+      <option class="bg-[white] border-0" value="Choose">انتخاب کنید</option>
+      <option class="bg-[white] border-0" value="Choose">کاربر ثبات</option>
+      <option class="bg-[white] border-0" value="Choose">کاربر اداره کل</option>
     </select>
     <div class="label-section flex justify-center items-center">
       <label class="text-center text-[16px]" for="select-input">انتخاب نقش</label>
