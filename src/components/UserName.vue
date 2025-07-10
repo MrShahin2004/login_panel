9c1f6901c9fe9c413fc1d9f2257f441ea241a3d6
Mr Shahin <hashmyshahin@gmail.com>
2025-07-10 08:48:58 +0330

    Added the Tailwind classes.

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="username-input bg-[#f3f4f6] h-fit grid grid-cols-[3fr_1fr] w-[90%]" id="username-input">
-    <input dir="rtl" type="text">
+    <input class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px]" dir="rtl" type="text">
     <div class="label-section flex justify-center items-center" id="label-section">
       <label class="text-center text-[16px]" for="text-input">نام کاربری</label>
     </div>
