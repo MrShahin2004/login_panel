210402d318b38aae3181dbc21f77951dc8406465
Mr Shahin <hashmyshahin@gmail.com>
2025-07-12 11:10:26 +0330

    Added the template and script here.

--- src/components/SecurityCode.vue
+++ src/components/SecurityCode.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="code-input bg-[#f3f4f6] w-90% h-fit">
-    <label for=""></label>
+  <div class="code-input bg-[#f3f4f6] w-[90%] h-fit grid grid-cols-[3fr_1fr_1.3fr]">
+    <input class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0" id="code-input" type="text">
+    <p class="flex justify-center items-center">{{ SomeNumber }}</p>
+    <label class="flex justify-center items-center" for="code-input">کد امنیتی</label>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      SomeNumber: null
+    };
+  },
+  mounted() {
+    console.log("🧠 Vue mounted");
+
+    fetch("/api/number?" + Date.now())
+        .then((res) => {
+          console.log("🛰️ Got response:", res);
+          return res.json();
+        })
+        .then((data) => {
+          console.log("✅ JSON parsed:", data);
+          this.SomeNumber = data.number;
+        })
+        .catch((err) => {
+          console.error("❌ Fetch error:", err);
+        });
+  }
+}
 </script>
 
-<style></style>
+<style>
+.code-input {
+  padding: 0.5rem;
+}
+
+input {
+  font-family: "B Nazanin", sans-serif;
+  padding: 0.5rem;
+}
+</style>
