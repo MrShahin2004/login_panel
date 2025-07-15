87de7bcec15952c3f3bfd83020e819184e3ba08a
Mr Shahin <hashmyshahin@gmail.com>
2025-07-15 09:19:22 +0330

    Update database.js
    Changed the path for "POST" method.

--- src/config/database.js
+++ src/config/database.js
@@ -50,7 +50,7 @@ App.get("/api/number", (req, res) => {
 });
 
 // POST method
-App.post("/api/data", async (req,
+App.post("/api/auth/register", async (req,
                              res) => {
     let RequestData = req.body;
     if (!RequestData.role || !RequestData.user || !RequestData.pass) {
