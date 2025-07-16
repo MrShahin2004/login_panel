a58e28e76682e75244efad6a03c84d4fcb9db679
Mr Shahin <hashmyshahin@gmail.com>
2025-07-16 14:08:48 +0330

    Variable to work with the value

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,8 @@ export default {
     return {
       ReceivedRole: "",
       ReceivedUser: "",
-      ReceivedPass: ""
+      ReceivedPass: "",
+      ReceivedCode: ""
     };
   },
   methods: {
