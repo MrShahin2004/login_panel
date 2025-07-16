60345a0fd706acfab93d11bd95f983fa224e63bc
Mr Shahin <hashmyshahin@gmail.com>
2025-07-16 14:07:23 +0330

    Added the element of CAPTCHA in the child component.

--- src/components/SecurityCode.vue
+++ src/components/SecurityCode.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="code-input bg-[#f3f4f6] w-[90%] h-fit grid grid-cols-[3fr_1fr_1.3fr] rounded-b-[6px_6px]">
-    <input class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0" id="code-input" type="text">
+    <input v-model="InsertedCode" @input="SendCode"
+           class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0"
+           id="code-input" type="text">
+    <div class="flex justify-center items-center">
+      <img class="cursor-pointer" v-bind:src="CaptchaLink" alt="captcha" @click="GetCaptcha">
+    </div>
     <label class="flex justify-center items-center" for="code-input">کد امنیتی</label>
   </div>
 </template>
