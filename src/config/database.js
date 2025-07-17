5608ff4675adcf3440de8df568646110a746e9a6
Mr Shahin <hashmyshahin@gmail.com>
2025-07-17 09:13:36 +0330

    Changed the API link for POST to a more proper name.

--- src/config/database.js
+++ src/config/database.js
@@ -69,8 +69,8 @@ App.get("/api/number", (req, res) => {
 });
 
 // POST method
-App.post("/api/auth/register", async (req,
-                             res) => {
+App.post("/api/mariadb/register", async (req,
+                                      res) => {
     let RequestData = req.body;
     if (!RequestData.role || !RequestData.user || !RequestData.pass) {
         return res.status(400).json({error: "Missing required fields"});
