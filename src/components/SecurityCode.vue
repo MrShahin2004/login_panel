7954f1ac581c607d066e3071434a083f417f4499
Mr Shahin <hashmyshahin@gmail.com>
2025-07-17 11:54:34 +0330

    Moved the conditions of the security code from "App.vue" to here.

--- src/components/SecurityCode.vue
+++ src/components/SecurityCode.vue
@@ -23,7 +23,16 @@ export default {
       this.CaptchaLink = "http://localhost:4000/api/captcha/get?t" + Date.now();
     },
     SendCode() {
-      this.$emit("SendCode", this.InsertedCode);
+      if (+this.InsertedCode.length === 0) {
+        console.log("Please insert a value.");
+      } else if (isNaN(+this.InsertedCode)) {
+        console.log("Security code must be only a number.");
+      } else if (+this.InsertedCode.length > 5 || +this.InsertedCode.length < 5) {
+        console.log("Security code must be exactly 5 digits.");
+      } else {
+        console.log("Security code meets the condition.");
+        this.$emit("SendCode", this.InsertedCode);
+      }
     }
   },
   mounted() {
