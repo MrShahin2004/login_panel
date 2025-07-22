9d5f282cc188e9834f7f238ff4470bec11fa72af
Mr Shahin <hashmyshahin@gmail.com>
2025-07-22 12:49:51 +0330

    Used it here.

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import Router from "./router/router"
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(Router);
+app.mount("#app");
