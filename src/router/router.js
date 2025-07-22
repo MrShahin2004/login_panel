c100711fe9d138b21b89408f9c320def501f4544
Mr Shahin <hashmyshahin@gmail.com>
2025-07-22 12:45:33 +0330

    Defined a module to reroute.

--- src/router/router.js
+++ src/router/router.js
@@ -1,1 +1,17 @@
 "use strict";
+
+import {createRouter, createWebHistory} from 'vue-router';
+import App from "../App.vue";
+import UserProfile from "../UserProfile.vue";
+
+const Routes = [
+    {path: "/", component: App, name: "Home"},
+    {path: "/new", component: UserProfile, name: "New"}
+];
+
+const Router = createRouter({
+    history: createWebHistory(),
+    Routes
+});
+
+export default Router;
