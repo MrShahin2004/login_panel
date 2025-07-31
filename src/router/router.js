30ebff1fd42560ba91b724db5847efbd66ecb385
Mr Shahin <hashmyshahin@gmail.com>
2025-07-31 17:03:16 +0330

    Added the new component to the router source.

--- src/router/router.js
+++ src/router/router.js
@@ -6,7 +6,8 @@ import AdminProfile from "@/views/AdminProfile.vue";
 const routes = [
     {path: "/", component: LogIn, name: "Home"},
     {path: "/user/profile/:username/:token", component: UserProfile, name: "UserProfile", props: true},
-    {path: "/admin/profile/:username/:token", component: AdminProfile, name: "AdminProfile", props: true}
+    {path: "/admin/profile/:username/:token", component: AdminProfile, name: "AdminProfile", props: true},
+    {path: "/register", component: SignIn, name: "SignIn", props: true}
 ];
 
 const router = createRouter({
