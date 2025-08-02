f0ba03735a1aefecd14c1e28e754e13278341838
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 13:52:08 +0330

    Put the elements of first name field (styles will be adjusted if needed).

--- src/views/SigninComps/FirstName.vue
+++ src/views/SigninComps/FirstName.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="flex justify-center items-center">
+    <div class="first-input select-box bg-[#f3f4f6] h-fit grid grid-cols-[3fr_1fr] w-[50%]">
+      <input class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0"
+             dir="rtl" type="text" id="user-input">
+      <div class="flex justify-center items-center">
+        <label for="user-input">نام</label>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script></script>
