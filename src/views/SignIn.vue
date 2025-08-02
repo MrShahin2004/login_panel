7fa9f3b1e52c4c35f7fdd9eafba87231b153bd9c
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 10:06:42 +0330

    Wrapped the whole content in a "div".

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>Welcome to the sign in page through Vue Router!</h1>
+  <div class="w-[100vw] h-[100vh] flex justify-center items-center">
+    <div class="w-[95%] h-[90%] bg-[white]">
+      <h1>Welcome to the sign in page through Vue Router!</h1>
+    </div>
   </div>
 </template>
 
