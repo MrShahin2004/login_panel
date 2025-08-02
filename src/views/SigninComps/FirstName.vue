5dac2cf5e75f296e54ac054a6fa197ca13ec77e1
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 21:00:53 +0330

    Some properties to send data

--- src/views/SigninComps/FirstName.vue
+++ src/views/SigninComps/FirstName.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex justify-center items-center">
     <div class="first-input bg-[#f3f4f6] select-box h-fit grid grid-cols-[3fr_1fr] w-[70%]">
-      <input class="bg-[#dbeafe] w-[100%] border-0 rounded-[6px] text-[16px] focus:outline-0"
-             dir="rtl" type="text" id="user-input">
+      <input @input="FirstSender"
+             class="bg-[#dbeafe] w-[100%] border-0 rounded-[6px] text-[16px] focus:outline-0"
+             dir="rtl" type="text" id="user-input" v-model="InsertedFirstName">
       <div class="flex justify-center items-center">
         <label for="user-input">نام</label>
       </div>
