0edb880e7a4f06093b3ca9f72fc5a898b7a747a9
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 11:57:29 +0330

    And used them here.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,6 +2,16 @@
   <div class="w-[100vw] h-[100vh] flex flex-col justify-evenly items-center">
     <h1 class="text-[24px]">ساخت حساب کاربری</h1>
     <div class="main-body w-[95%] h-[85%] bg-[var(--body-bg)]">
+      <div class="fields-container">
+        <FirstName/>
+        <LastName/>
+        <NationalID/>
+        <UserName/>
+        <PassWord/>
+        <EmailAddress/>
+        <FirmName/>
+        <TypeSelect/>
+      </div>
       <button @click="ReturnLogIn">بازگشت به صفحه ورود</button>
     </div>
   </div>
