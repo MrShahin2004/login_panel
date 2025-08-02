04570577d0f6c38e00242fad3fa73e086664d0e4
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 11:57:08 +0330

    Imported the components.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -8,6 +8,14 @@
 </template>
 
 <script>
+import EmailAddress from "@/views/SigninComps/EmailAddress.vue";
+import FirmName from "@/views/SigninComps/FirmName.vue";
+import FirstName from "@/views/SigninComps/FirstName.vue";
+import LastName from "@/views/SigninComps/LastName.vue";
+import NationalID from "@/views/SigninComps/NationalID.vue";
+import PassWord from "@/views/SigninComps/PassWord.vue";
+import TypeSelect from "@/views/SigninComps/TypeSelect.vue";
+import UserName from "@/views/SigninComps/UserName.vue";
 import {useRouter} from "vue-router";
 
 export default {
