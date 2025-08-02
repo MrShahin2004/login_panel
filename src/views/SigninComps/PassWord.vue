fb6f6de75307443d595b8cb861e049b716596b6c
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 14:20:15 +0330

    Copied and pasted the elements of FirstName.vue to see the test styles (each will get its own elements later).

--- src/views/SigninComps/PassWord.vue
+++ src/views/SigninComps/PassWord.vue
@@ -1,7 +1,24 @@
 <template>
-  <div></div>
+  <div class="flex justify-center items-center">
+    <div class="first-input select-box bg-[#f3f4f6] h-fit grid grid-cols-[3fr_1fr] w-[50%]">
+      <input class="bg-[#dbeafe] border-0 rounded-[6px] text-[16px] focus:outline-0"
+             dir="rtl" type="text" id="user-input">
+      <div class="flex justify-center items-center">
+        <label for="user-input">نام</label>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+</script>
 
-<style></style>
+<style scoped>
+.first-input {
+  padding: 0.5rem;
+}
+
+label {
+  font-family: "B Nazanin", cursive;
+}
+</style>
