c3ba9b629e1b99905442d6cec9ecb4859d2b02de
Mr Shahin <hashmyshahin@gmail.com>
2025-08-02 10:19:23 +0330

    Added the background color.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-[100vw] h-[100vh] flex justify-center items-center">
-    <div class="w-[95%] h-[90%] bg-[white]">
+    <div class="w-[95%] h-[90%] bg-[var(--body-bg)]">
       <h1>Welcome to the sign in page through Vue Router!</h1>
     </div>
   </div>
