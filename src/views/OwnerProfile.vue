5eeb967d1b729032b5cb1f7305d7cb2a43309fc7
Mr Shahin <hashmyshahin@gmail.com>
2025-08-07 10:28:55 +0330

    Wrote some template for testing (and it works, will be changed later).

--- src/views/OwnerProfile.vue
+++ src/views/OwnerProfile.vue
@@ -1,7 +1,33 @@
 <template>
-  <div></div>
+  <div class="w-[100vw] h-[100vh]">
+    <h1>Welcome to your own panel, {{ username }}!</h1>
+    <h1>You are the owner of this app.</h1>
+    <button
+        @click="ReturnToLogin"
+        class="text-[white] bg-[var(--btn-bg)] rounded-[6px] cursor-pointer"
+        style="padding: 0.5rem">Back to login
+    </button>
+  </div>
 </template>
 
-<script></script>
+<script>
+import {useRouter} from "vue-router";
+
+export default {
+  name: "OwnerProfile",
+  props: {
+    username: String
+  },
+  setup() {
+    let Router = useRouter()
+
+    function ReturnToLogin() {
+      Router.push({name: "Home"});
+    }
+
+    return {ReturnToLogin};
+  }
+};
+</script>
 
 <style></style>
