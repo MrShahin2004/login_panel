2a387c67f75c805c4bb6e5b75d9d33cde0afac61
Mr Shahin <hashmyshahin@gmail.com>
2025-08-08 17:23:28 +0330

    Added inline font family.

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -3,7 +3,9 @@
     <div class="flex justify-between relative">
       <img class="w-[160px]" src="@/assets/images/khamenei.png" alt="khamenei_image">
       <h1 class="text-[16px] absolute left-[50%] top-[90px] translate-x-[-50%] translate-y-[-100%]
-                 text-shadow-[0_4px_10px_black]">پایگاه اطلاعات قراردادهای اداره کل آموزش و پرورش استان خراسان رضوی</h1>
+                 text-shadow-[0_4px_10px_black]"
+          style="font-family: 'B Titr', 'cursive'">
+        پایگاه اطلاعات قراردادهای اداره کل آموزش و پرورش استان خراسان رضوی</h1>
       <img class="w-[160px]" src="@/assets/images/education_logo.png" alt="education_image">
     </div>
     <div class="login-body w-full h-full flex justify-center items-center">
