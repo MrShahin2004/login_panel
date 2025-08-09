706a5c0ee30abdf46d6663f0d36aa81adce4cd1f
Mr Shahin <hashmyshahin@gmail.com>
2025-08-09 12:32:04 +0330

    Divided in two sections.

--- src/views/OwnerProfile.vue
+++ src/views/OwnerProfile.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="w-[100vw] h-[100vh]">
-    <h1>Welcome to your own panel, {{ username }}!</h1>
-    <h1>You are the owner of this app.</h1>
-    <button
-        @click="ReturnToLogin"
-        class="text-[white] bg-[var(--btn-bg)] rounded-[6px] cursor-pointer"
-        style="padding: 0.5rem">Back to login
-    </button>
+  <div class="w-[100vw] h-[100vh] flex justify-center items-center">
+    <div class="side-panel w-[20%] h-[100%]"></div>
+    <div class="main-content w-[80%] h-[100%]">
+      <h1>Welcome to your own panel, {{ username }}!</h1>
+      <h1>You are the owner of this app.</h1>
+      <button
+          @click="ReturnToLogin"
+          class="text-[white] bg-[var(--btn-bg)] rounded-[6px] cursor-pointer"
+          style="padding: 0.5rem">Back to login
+      </button>
+    </div>
   </div>
 </template>
 
