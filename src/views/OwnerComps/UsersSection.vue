8b62c4dffc1020b97feb17242fa42198f79a68a8
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-10 13:09:07 +0330

    Built the base structure of the users section.

--- src/views/OwnerComps/UsersSection.vue
+++ src/views/OwnerComps/UsersSection.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="w-[100%] h-[100%] flex justify-center items-center">
+    <div class="main-container w-[90%] h-[93%] rounded-[6px]">
+      <div class="title-div w-[100%] h-[10%]">
+        <h2 class="text-[24px]">فهرست کاربران عادی <i class="fas fa-user"></i></h2>
+      </div>
+      <div class="button-div w-[100%] h-[10%]"></div>
+      <div class="content-div w-[100%] h-[80%]"></div>
+    </div>
+  </div>
 </template>
 
 <script>
