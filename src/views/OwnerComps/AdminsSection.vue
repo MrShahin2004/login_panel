d566c571ac60830ef1eeddbab1317de7113f84b6
MrShahin2004 <hashmyshahin@gmail.com>
2025-08-11 12:37:56 +0330

    Forgot the icon lol.

--- src/views/OwnerComps/AdminsSection.vue
+++ src/views/OwnerComps/AdminsSection.vue
@@ -2,7 +2,7 @@
   <div class="w-[100%] h-[100%] flex justify-center items-center">
     <div class="main-container w-[90%] h-[93%] rounded-[6px] border border-black border-solid">
       <div class="title-div w-[100%] h-[10%] flex justify-center items-center">
-        <h2 class="text-[24px]">فهرست ادمین ها <i class="fas fa-user"></i></h2>
+        <h2 class="text-[24px]">فهرست ادمین ها <i class="fas fa-user-shield"></i></h2>
       </div>
       <div class="button-div w-[100%] h-[10%] flex justify-center items-center">
         <button
